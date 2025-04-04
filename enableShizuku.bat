c01d4a2f400265b9baf7e3c6dd21e675a38e0f19
Alex Nelson <alex.nelson167@gmail.com>
2025-04-04 09:21:55 -0500

    removed extra periods

--- enableShizuku.bat
+++ enableShizuku.bat
@@ -3,7 +3,7 @@ set dir= rem add path to ADB here
 cd %dir%
 .\adb.exe devices
 
-echo to enable Shizuku..
+echo to enable Shizuku
 pause
 
 .\adb.exe shell sh /storage/emulated/0/Android/data/moe.shizuku.privileged.api/start.sh
